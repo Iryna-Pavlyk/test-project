346e2acf0e610d7c094a848ef51ec7a85d1406b1
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-03-11 17:36:47 +0200

    s

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const drugsList = document.querySelector(".drugs-list");
 const list = document.querySelector(".total-list-cart");
+const btnSubmit = document.querySelector(".btn-submit-cart");
+const formData = document.querySelector(".form-data");
+const BASE_URL = "http://127.0.0.1:5500/index.html";
 const localeStorageKey = "key";
 
 drugsList.addEventListener("click", (event) => {
@@ -10,10 +13,39 @@ drugsList.addEventListener("click", (event) => {
 
 function getItem() {
   const localStorageValue = JSON.parse(localStorage.getItem(localeStorageKey));
-  console.log(localStorageValue);
   let markup = `<li class="drugs-item"><img src="./img/pill-min.jpg" alt="">
                             <h3 class="drugs-title">${localStorageValue}</h3>`;
   if (localStorageValue) {
     return list.insertAdjacentHTML("beforeend", markup);
   }
 }
+
+formData.addEventListener("input", inputHandler);
+
+let values;
+function inputHandler(event) {
+  const name = event.target.value;
+  const email = event.target.value;
+  const phone = event.target.value;
+  const address = event.target.value;
+  values = [name, email, phone, address];
+  getData();
+}
+
+function getData() {
+  fetch(BASE_URL).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText || response.status);
+    }
+    return response.json();
+  });
+}
+
+// btnSubmit.addEventListener("submit", submitHandler);
+
+// function submitHandler(event) {
+//   event.preventDefault();
+//   getData();
+// }
+
+// function getData() {}
